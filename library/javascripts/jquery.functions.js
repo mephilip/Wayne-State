9a0a9f7ce39a55a55459a68b007993c027e456f6
TBRudy3 <thomasrudy@pk-gajdaj.lib.wayne.edu>
2015-02-13 09:07:10 -0500

    Coda Coda Coda

--- library/javascripts/jquery.functions.js
+++ library/javascripts/jquery.functions.js
@@ -29,7 +29,24 @@ jQuery(document).ready(function($) {
 		}
 		$( ".flex-nav" ).scrollTop( 0 );
 	});
-	
+	$('#flex-overlay').click(function(){
+		if($('.flex-btn').hasClass('flex-active')){
+			$('.flex-btn').removeClass('flex-active');
+			$('.flex-nav').removeClass('flex-expand');
+			$('#container').removeClass('container-expand');
+			$('body').removeClass('no-scroll');
+			$('#nav-toggle').removeClass( "active" );
+			$('#flex-overlay').hide();
+		} else {
+			$(this).addClass('flex-active');
+			$('.flex-nav').addClass('flex-expand');
+			$('#container').addClass('container-expand');
+			$('body').addClass('no-scroll');
+			$('#flex-overlay').fadeIn();
+		}
+		$( ".flex-nav" ).scrollTop( 0 );
+	});
+		
 	$( "#nav-toggle" ).click(function() {
 		$( this ).toggleClass( "active" );
 	});
@@ -41,4 +58,11 @@ jQuery(document).ready(function($) {
 		}, 700);
 	});
 	
+	$(".nav-item", this).hoverIntent(function () { 
+		$(this).addClass('nav-active');
+		$(".sub-nav", this).slideDown(250);
+	}, function() {
+		$(".sub-nav", this).slideUp("fast"); 
+		$(this).removeClass('nav-active');
+	});
 });
--- library/partials/header/search/index.php
+++ library/partials/header/search/index.php
@@ -8,8 +8,36 @@
 	<div id="nav-toggle" class="flex-btn"><span></span></div>
 	<header>
 		<div id="page-header">
-			<img id="logo" src="http://<?php echo $_SERVER[HTTP_HOST]?>/test/thomas/library/path-get/assets/images/wsu-shield.png" alt="wsu-shield" width="482" height="424">
+			<div id="header-wrap">
+				<div id="contents">
+					<img id="logo" src="http://<?php echo $_SERVER[HTTP_HOST]?>/test/thomas/library/path-get/assets/images/wsu-shield.png" alt="wsu-shield" width="482" height="424">
+					
+					<nav id="page-nav">
+						<ul class="nav main-nav parent-nav">
+						  <li role="presentation" class="nav-item">
+						  		<a href="#">Resources</a>
+						  		<ul class="sub-nav">
+						  			<li><a href="">Resource</a></li>
+						  			<li><a href="">Resource</a></li>
+						  			<li><a href="">Resource</a></li>
+								</ul>
+						  </li>
+						  <li role="presentation" class="nav-item">
+						  		<a href="#">Services</a>
+						  		<ul class="sub-nav">
+						  			<li><a href="">Service</a></li>
+						  			<li><a href="">Service</a></li>
+						  			<li><a href="">Service</a></li>
+								</ul>
+						  </li>
+						  <li role="presentation"><a href="#">Libraries</a></li>
+						  <li role="presentation"><a class="icon" href="#"><i class="fa fa-comment"></i> Ask-A-Librarian</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
 		</div>
+		
 	</header>
 	<section id="panel-content">
 	
@@ -17,10 +45,11 @@
 	</section>
 
 </div>
-<div id="" class="full-panel login-panel">
+<div id="" class="full-panel">
 	
 	<section id="panel-content">
 	
+		<h1>"Imagine all the content ... " ~ John Lennon</h1>
 	
 	</section>
 
