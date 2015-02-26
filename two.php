28dad1899663e79dd8fdeb69b26c59534aec535c
TBRudy3 <thomasbrudy@gmail.com>
2015-02-26 16:55:07 -0500

    I live in the console for these days ....

--- two.php
+++ two.php
@@ -7,3 +7,24 @@ header('Content-Type: text/plain');
 // Send the data back.
 echo "Page Two";
 ?>
+
+<script>
+var xhr = new XMLHttpRequest();
+xhr.open('get', 'two.php');
+ 
+// Track the state changes of the request.
+xhr.onreadystatechange = function () {
+    var DONE = 4; // readyState 4 means the request is done.
+    var OK = 200; // status 200 is a successful return.
+    if (xhr.readyState === DONE) {
+        if (xhr.status === OK) {
+            window.innerHTML = xhr.responseText; // 'This is the returned text.'
+        } else {
+            alert('Error: ' + xhr.status); // An error occurred during the request.
+        }
+    }
+};
+ 
+// Send the request to send-ajax-data.php
+xhr.send(null);
+</script>
