6d27d3c996306c5b59c1ea1c299368ba154df23d
TBRudy3 <thomasbrudy@gmail.com>
2015-02-26 16:54:18 -0500

    Getting there

---
+++ one.php
@@ -0,0 +1,32 @@
+<?php
+// This is the server-side script.
+ 
+// Set the content type.
+header('Content-Type: text/plain');
+ 
+// Send the data back.
+echo "Page One";
+
+
+?>
+
+<script>
+	var xhr = new XMLHttpRequest();
+xhr.open('get', 'two.php');
+ 
+// Track the state changes of the request.
+xhr.onreadystatechange = function () {
+    var DONE = 4; // readyState 4 means the request is done.
+    var OK = 200; // status 200 is a successful return.
+    if (xhr.readyState === DONE) {
+        if (xhr.status === OK) {
+            window.innerHTML = xhr.responseText; // 'This is the returned text.'
+        } else {
+            alert('Error: ' + xhr.status); // An error occurred during the request.
+        }
+    }
+};
+ 
+// Send the request to send-ajax-data.php
+xhr.send(null);
+</script>
